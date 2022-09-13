3425f8025744cf6ccb6c547a75641f49caf93e9f
unknown <huyleezoan@gmail.com>
2022-09-13 17:18:32 +0700

    up

--- Day 28/pomodoro-start/main.py
+++ Day 28/pomodoro-start/main.py
@@ -1,3 +1,4 @@
+import math
 import tkinter
 
 # ---------------------------- CONSTANTS ------------------------------- #
@@ -9,13 +10,39 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
+timer = None
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
+def reset_timer():
+    window.after_cancel(timer)
+    text_timer.config(text="Timer")
+    canvas.config(tomato_time="00:00")
+    check_mark.config(text="")
+    global reps
+    reps = 0
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
-    count_down(5 * 60)
+    global reps
+    reps += 1
+    work_sec = 10
+    short_break_sec = 3
+    long_break_sec = 5
+
+    # text_timer.config(foreground=GREEN)
+    # count_down(work_sec)
+    if reps % 8 == 0:
+        text_timer.config(foreground=RED, text="BREAK")
+        count_down(long_break_sec)
+    elif reps % 2 == 0:
+        text_timer.config(foreground=PINK, text="BREAK")
+        count_down(short_break_sec)
+    else:
+        text_timer.config(foreground=GREEN, text="WORK")
+        count_down(work_sec)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
@@ -29,11 +56,21 @@ import time
 
 def count_down(count):
     # "00:00"
-    count_min = count / 60
-    canvas.itemconfig(tomato_time, text=count_min)
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = "0" + str(count_sec)
+    canvas.itemconfig(tomato_time, text=f"{count_min}:{count_sec}")
 
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        if reps % 2 == 0:
+            completed_task = int(reps / 2)
+            global check_mark
+            check_mark.config(text=''.join(["✓" for _ in range(1, (completed_task + 1))]))
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -63,11 +100,11 @@ canvas.grid(row=1, column=1)
 start_button = tkinter.Button(text="Start", background="white", highlightthickness=0, command=start_timer)
 start_button.grid(row=2, column=0)
 
-reset_button = tkinter.Button(text="Reset", background="white", highlightthickness=0)
+reset_button = tkinter.Button(text="Reset", background="white", highlightthickness=0, command=reset_timer)
 reset_button.grid(row=2, column=2)
 
 # Create check
-check_mark = tkinter.Label(text="✓", foreground=GREEN, background=YELLOW, font=(FONT_NAME, 15))
+check_mark = tkinter.Label(foreground=GREEN, background=YELLOW, font=(FONT_NAME, 15))
 check_mark.grid(column=1, row=3)
 
 window.mainloop()
