b5c64a2ae4d79ccd5965644a3302c7caba7a717a
unknown <huyleezoan@gmail.com>
2022-10-16 10:55:01 +0700

    up haha

--- Day 50/auto_tinder_swipping_bot.py
+++ Day 50/auto_tinder_swipping_bot.py
@@ -11,14 +11,15 @@ PASS = os.environ['PASSWORD']
 
 URL = "https://tinder.com"
 
-service = Service("../../../../../../ASUS/Documents/chromedriver.exe")
+# service = Service("../../../../../../ASUS/Documents/chromedriver.exe")
 option = Options()
-option.add_experimental_option("debuggerAddress", "localhost:9111")
+# option.add_experimental_option("debuggerAddress", "localhost:9111")
 
-os.popen(
-    '"C:\Program Files\Google\Chrome\Application\chrome.exe" --remote-debugging-port=9111 --user-data-dir="C:\\Users\ASUS\Documents"')
+# os.popen(
+#     '"C:\Program Files\Google\Chrome\Application\chrome.exe" --remote-debugging-port=9111 --user-data-dir="C:\\Users\ASUS\Documents"')
 
-driver = webdriver.Chrome(executable_path="C:\\Users\ASUS\Documents\chromedriver.exe", options=option)
+driver = webdriver.Chrome(
+    executable_path="./driver/chromedriver.exe")
 driver.maximize_window()
 driver.get(URL)
 
@@ -28,23 +29,23 @@ driver.get(URL)
     Login Google Account
 """
 
-time.sleep(2)
-login_button = driver.find_element(By.XPATH,
-                                   '//*[@id="t1452431810"]/div/div[1]/div/main/div[1]/div/div/div/div/header/div/div[2]/div[2]/a/div[2]/div[2]')
-login_button.click()
+# time.sleep(2)
+# login_button = driver.find_element(By.XPATH,
+#                                    '//*[@id="t1452431810"]/div/div[1]/div/main/div[1]/div/div/div/div/header/div/div[2]/div[2]/a/div[2]/div[2]')
+# login_button.click()
 
-time.sleep(2)
-google_button = driver.find_element(By.XPATH,
-                                    '//*[@id="t-275949266"]/main/div/div[1]/div/div/div[3]/span/div[1]/div/button/div[2]/div[2]')
-google_button.click()
+# time.sleep(2)
+# google_button = driver.find_element(By.XPATH,
+#                                     '//*[@id="t-275949266"]/main/div/div[1]/div/div/div[3]/span/div[1]/div/button/div[2]/div[2]')
+# google_button.click()
 
-time.sleep(2)
+# time.sleep(2)
 
-base_window = driver.window_handles[0]
-google_login_window = driver.window_handles[1]
-driver.switch_to.window(google_login_window)
+# base_window = driver.window_handles[0]
+# google_login_window = driver.window_handles[1]
+# driver.switch_to.window(google_login_window)
 
-time.sleep(2)
-mail_input = driver.find_element(By.ID, 'identifierId')
-mail_input.send_keys(MAIL)
-mail_input.send_keys(Keys.ENTER)
+# time.sleep(2)
+# mail_input = driver.find_element(By.ID, 'identifierId')
+# mail_input.send_keys(MAIL)
+# mail_input.send_keys(Keys.ENTER)
