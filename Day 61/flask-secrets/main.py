0c0bc36fdb3151ef5dfd889c23253f75f3688a1a
unknown <huyleezoan@gmail.com>
2022-10-17 11:39:16 +0700

    up

--- Day 61/flask-secrets/main.py
+++ Day 61/flask-secrets/main.py
@@ -1,15 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired
+from flask_bootstrap import Bootstrap
+
+
+class LoginForm(FlaskForm):
+    email = StringField(label='Email: ')
+    password = PasswordField(label='Password: ')
+    submit = SubmitField(label='Log In')
+
 
 app = Flask(__name__)
+Bootstrap(app)
+
+app.config['SECRET_KEY'] = 'hehe'
 
 
 @app.route("/")
 def home():
     return render_template('index.html')
 
-@app.route("/login")
+
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@gmail.com" and form.password.data == "12345678":
+            return render_template('success.html')
+        else:
+            return render_template('denied.html')
+    return render_template('login.html', form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
